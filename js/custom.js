6789ac0cb1a76cc780a96404e4d170e426df2706
Jay Makwana <jm4184995@gmail.com>
2025-06-16 08:46:27 +0530

    adding file

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+ document.addEventListener(
+                "DOMContentLoaded", () => {
+                    new Mmenu( "#menu", {
+                       "offCanvas": {
+                          "position": "right-front"
+                       },
+                       "theme": "dark"
+                    });
+                }
+            );
